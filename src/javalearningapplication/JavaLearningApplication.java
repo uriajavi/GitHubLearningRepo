bedd8003fe9d4398463be6a3cf734413bf5a78b4
jmarturi <jmarturi@HZ365738.TartangaLH.eus>
2022-09-16 09:06:00 +0200

    Initial commit for first application.

---
+++ src/javalearningapplication/JavaLearningApplication.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javalearningapplication;
+
+/**
+ *
+ * @author jmarturi
+ */
+public class JavaLearningApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if(args[1].equals("FILE"));
+    }
+    
+}
